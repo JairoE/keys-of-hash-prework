15a796064bd0d7810d0c912280fd64a3650ed593
Jairo Espinosa <jairoespinosa95@gmail.com>
2018-01-23 15:01:26 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,15 @@
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+    array = []
+
+    Hash.keys do |key|
+      if arguments.include?(key)
+        array << key
+      end
+    end
+
+    array
+
   end
 end
